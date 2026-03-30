751d3de5e08776966ebb446e52036c9a34d1afc9
Ziyan Gupta <ziyanrgupta777@gmail.com>
2026-03-30 23:03:06 +0530

    Program:Added fundamental booster

---
+++ fundamental booster.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+birth_year = 2026 - age
+print("Birth year:", birth_year)
+
+print(float(age))
+print(int(height))
+print("Type of height:", type(height))
+print("Memory of height:", id(height))
